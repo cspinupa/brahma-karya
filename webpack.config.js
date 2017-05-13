c7395d6f62e7816eb9df146f4ba68a67e21f5110
Chetan Pinupa <chetan.pinupa@gmail.com>
2017-05-14 02:00:14 +0530

    Modified the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,47 @@
 var path = require('path');
-
+var webpack = require('webpack');
+ 
 module.exports = {
-  entry : './app/index.js',
-  output : {
-    filename : 'bundle.js',
-    path : path.resolve(__dirname, 'dist')
-  }
-}
+  entry: './main.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      { 
+        test: /\.css$/, 
+        loader: "style-loader!css-loader" 
+      },
+      { 
+        test: /\.png$/, 
+        loader: "url-loader?limit=100000" 
+      },
+      { 
+        test: /\.jpg$/, 
+        loader: "file-loader" 
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'file'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      }
+    ]
+  },
+};
